8482cbf7731c2fbbfee07c7de026cca7a78c9b02
Farzeen <happycoder97@protonmail.com>
2019-09-16 08:32:17 +0530

    Port all windows.

--- src/ui.rs
+++ src/ui.rs
@@ -15,129 +15,103 @@ impl UI {
         imgui::Window::new(im_str!("Code"))
             .size([300.0, 100.0], Condition::FirstUseEver)
             .build(ui, || {
-                let [_, y_px] = ui.window_size();
-                let lines = ((y_px - 70.0)/20.0) as usize;
-                dbg!(y_px, lines);
+                let lines = 30;
                 let (base, ip, code_lines) = self.xsm.get_code(lines);
                 for (i, code) in code_lines.iter().enumerate() {
                     let instr_addr = base + 2 * i;
                     if instr_addr == ip {
-                        ui.text(im_str!(">> {}: {}", instr_addr, code));
+                        imgui::MenuItem::new(&im_str!("{}: {} ---- (IP)", instr_addr, code)).build(ui);
                     } else {
-                        ui.text(im_str!("{}: {}", instr_addr, code));
+                        imgui::MenuItem::new(&im_str!("{}: {}", instr_addr, code)).build(ui);
                     }
                 }
             });
     }
 
-    // fn render_regs1(&mut self) {
-    //     let window = &mut self.window_regs1;
-    //     window.clear();
+    fn render_regs1(&mut self, ui: &mut Ui) {
+        imgui::Window::new(im_str!("Registers"))
+            .size([300.0, 100.0], Condition::FirstUseEver)
+            .build(ui, || {
+                for i in 0..=15usize {
+                    ui.text(format!("R{}: {}", i, &self.xsm.get_regs().r[i]));
+                }
+                ui.separator();
+                for i in 15..20usize {
+                    ui.text(format!("R{}: {}", i, &self.xsm.get_regs().r[i]));
+                }
+            });
+    }
 
-    //     window.text("REGISTERS");
-    //     window.hline();
+    fn render_regs2(&mut self, ui: &mut Ui) {
+        imgui::Window::new(im_str!("Registers Extra"))
+            .size([300.0, 100.0], Condition::FirstUseEver)
+            .build(ui, || {
+                ui.text("PORTS");
+                ui.separator();
 
-    //     for i in 0..=15usize {
-    //         window.text(format!("R{}: {}", i, &self.xsm.get_regs().r[i]));
-    //     }
+                for i in 0..4usize {
+                    ui.text(format!("P{}: {}", i, &self.xsm.get_regs().p[i]));
+                }
 
-    //     for i in 15..20usize {
-    //         window.text(format!("R{}: {}", i, &self.xsm.get_regs().r[i]));
-    //     }
+                ui.new_line();
+                ui.text("STACK");
+                ui.separator();
+                ui.text(format!("BP: {}", &self.xsm.get_regs().bp));
+                ui.text(format!("SP: {}", &self.xsm.get_regs().sp));
 
-    //     window.render();
-    // }
+                ui.new_line();
+                ui.text("PAGE TABLE");
+                ui.separator();
+                ui.text(format!("PTBR: {}", &self.xsm.get_regs().ptbr));
+                ui.text(format!("PTLR: {}", &self.xsm.get_regs().ptlr));
 
-    // fn render_regs2(&mut self) {
-    //     let window = &mut self.window_regs2;
-    //     window.clear();
+                ui.new_line();
+                ui.text("OTHERS");
+                ui.separator();
+                ui.text(format!("IP: {}", &self.xsm.get_regs().ip));
+                ui.text(format!("EIP: {}", &self.xsm.get_regs().eip));
+                ui.text(format!("EC: {}", &self.xsm.get_regs().ec));
+                ui.text(format!("EPN: {}", &self.xsm.get_regs().epn));
+                ui.text(format!("EMA: {}", &self.xsm.get_regs().epn));
+            });
+    }
 
-    //     window.hline();
-    //     window.text("PORTS");
-    //     window.hline();
+    fn render_page_table(&mut self, ui: &mut Ui) {
+        imgui::Window::new(im_str!("Page Table"))
+            .size([300.0, 100.0], Condition::FirstUseEver)
+            .build(ui, || {
+                for (i, entry) in self.xsm.get_page_table().iter().enumerate() {
+                    ui.text(format!("{} -> {}    [{}]", i, entry.phy, entry.aux))
+                }
+            });
+    }
 
-    //     for i in 0..4usize {
-    //         window.text(format!("P{}: {}", i, &self.xsm.get_regs().p[i]));
-    //     }
+    fn render_errors(&mut self, ui: &mut Ui) {
+        imgui::Window::new(im_str!("Errors"))
+            .size([300.0, 100.0], Condition::FirstUseEver)
+            .build(ui, || {
+                for error in self.xsm.get_errors() {
+                    ui.text(format!("{:#?}", error))
+                }
+            });
+    }
 
-    //     window.hline();
-    //     window.text("STACK");
-    //     window.hline();
-    //     window.text(format!("BP: {}", &self.xsm.get_regs().bp));
-    //     window.text(format!("SP: {}", &self.xsm.get_regs().sp));
-
-    //     window.hline();
-    //     window.text("PAGE TABLE");
-    //     window.hline();
-    //     window.text(format!("PTBR: {}", &self.xsm.get_regs().ptbr));
-    //     window.text(format!("PTLR: {}", &self.xsm.get_regs().ptlr));
-
-    //     window.hline();
-    //     window.text("OTHERS");
-    //     window.hline();
-    //     window.text(format!("IP: {}", &self.xsm.get_regs().ip));
-    //     window.text(format!("EIP: {}", &self.xsm.get_regs().eip));
-    //     window.text(format!("EC: {}", &self.xsm.get_regs().ec));
-    //     window.text(format!("EPN: {}", &self.xsm.get_regs().epn));
-    //     window.text(format!("EMA: {}", &self.xsm.get_regs().epn));
-
-    //     window.render();
-    // }
-
-    // fn render_page_table(&mut self) {
-    //     let window = &mut self.window_page_table;
-    //     window.clear();
-
-    //     window.text("PAGE TABLE");
-    //     window.hline();
-
-    //     for (i, entry) in self.xsm.get_page_table().iter().enumerate() {
-    //         window.text(format!("{} -> {}    [{}]", i, entry.phy, entry.aux))
-    //     }
-    //     window.render();
-    // }
-
-    // fn render_errors(&mut self) {
-    //     let window = &mut self.window_errors;
-    //     window.clear();
-
-    //     window.text("ERRORS");
-    //     window.hline();
-
-    //     for error in self.xsm.get_errors() {
-    //         window.text(format!("{:#?}", error))
-    //     }
-    //     window.render();
-    // }
-
-    // fn render_output(&mut self) {
-    //     let window = &mut self.window_output;
-    //     window.clear();
-
-    //     window.text("OUTPUT");
-    //     window.hline();
-    //     for line in self.xsm.get_output() {
-    //         window.text(&line);
-    //     }
-    //     window.render();
-    // }
+    fn render_output(&mut self, ui: &mut Ui) {
+        imgui::Window::new(im_str!("Output"))
+            .size([300.0, 100.0], Condition::FirstUseEver)
+            .build(ui, || {
+                for line in self.xsm.get_output() {
+                    ui.text(&line);
+                }
+            });
+    }
 
     pub fn render_all(&mut self, ui: &mut Ui) {
         self.render_code(ui);
-        // self.render_regs1();
-        // self.render_regs2();
-        // self.render_page_table();
-        // self.render_errors();
-        // self.render_output();
+        self.render_regs1(ui);
+        self.render_regs2(ui);
+        self.render_page_table(ui);
+        self.render_errors(ui);
+        self.render_output(ui);
     }
-
-    // #[allow(dead_code)]
-    // pub fn wait_for_keypress(&mut self) {
-    //     self.stdscr.getch();
-    // }
-
-    // #[allow(dead_code)]
-    // pub fn exit(&mut self) {
-    //     pancurses::endwin();
-    // }
 }
